cb82d3706f1939c5bd416101be7a3d95794cb90d
devbeni <ballobeni08@gmail.com>
2025-12-14 23:58:51 +0100

    fix: Add missing using directive for Riptide.Demos.Steam.PlayerHosted in SaveTransferPacket

--- Packets/Lobby/SaveTransferPacket.cs
+++ Packets/Lobby/SaveTransferPacket.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Riptide.Demos.Steam.PlayerHosted;
 using static KCM.Main;
 
 namespace KCM.Packets.Lobby
